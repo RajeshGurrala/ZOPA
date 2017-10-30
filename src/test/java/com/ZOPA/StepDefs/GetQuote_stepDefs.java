18beb980aa275efb69cb297619fb248bfb8e1ba1
rajeshg <rajesh.gurrala@unionstreet.uk.com>
2017-10-30 07:52:40 +0000

    CreateQuote

--- src/test/java/com/ZOPA/StepDefs/GetQuote_stepDefs.java
+++ src/test/java/com/ZOPA/StepDefs/GetQuote_stepDefs.java
@@ -27,7 +27,7 @@ public class GetQuote_stepDefs {
         //this method will assert the user is on https://www.zopa.com/loans page. a text on the page is asserted
         webModel.loansPage().assertLoansPage();
         //this method will provide the loan amount and choose the desired term duration.
-        // the loan amount provided is via random method
+        // the loan amount provided is generated via random method ( JAVA random utils)
         webModel.loansPage().chooseLoanAmount();
         //the term duration is also picked in random. i.e each time a test runs a different number between 1-5 is passed
         webModel.loansPage().chooseYear();
@@ -40,10 +40,19 @@ public class GetQuote_stepDefs {
     public void heShouldBeDirectedToASecuredLinkWhereHeWouldBeAbleToProvideHisPersonalDetails() throws InterruptedException {
         //this method will assert that the flow is on the personalisedLoanPage. a text on the page is asserted
         webModel.getPersonalisedLoanRatesPage().assertPersonalisedLoanPage();
-        //this method will populate fields under about you label
-        // all data provided is random
+        //this method will populate fields under 'about you' label
+        // email, first name, last name and phone number provided by random methods, title is selected at random
+        // date and month are hard coded however year is calculated by a method that gets current year and then subtracts 21 out of it and provides the value
+        //loan reason like title is selected at random for each run,
         webModel.getPersonalisedLoanRatesPage().populateAboutYou();
+        //address history is populated by a method that stores few pre assigned address and provides them at random per each test execution
+        //month and year are chosen by a method are provided by selectByIndex
         webModel.getPersonalisedLoanRatesPage().populateAddressHistory();
+        //financial details again like title and loan reason are chosen at random per each test run
+        //annual salary is provided by random method
+        //for 'do you own home': initially 'either of 2nd and 3rd radio buttons are chosen at random and then a amount is provided in the field below
+        //assertion of the value that is entered is performed on the string 'You have entered £xxxx.'
+        //password is provided by combination of hardcoded and random alphanumerics
         webModel.getPersonalisedLoanRatesPage().financialDetails();
 
 
